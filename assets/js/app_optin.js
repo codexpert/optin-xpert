0549c09eb644a0dcba5cc5cd4cdbb2fa0111ede8
masud <masud.ahmed92@gmail.com>
2015-03-18 19:30:24 +0600

    waypoint feature added

--- assets/js/app_optin.js
+++ assets/js/app_optin.js
@@ -1,43 +1,58 @@
  (function($) {
-
-
  $(document).ready(function () {
 
- 	
-
-// (function($) {
-//  	 $(document).ready(function () {
 
 
-//  	 alert('test');
+ // if(!$('#sidebar-pagebottom').hasClass('introduction')) {
+ //            $('#sidebar-pagebottom').addClass('introduction');
+
+ //        }
 
 
- if(!$('#sidebar-pagebottom').hasClass('introduction')) {
-            $('#sidebar-pagebottom').addClass('introduction');
+$('footer')
+   .waypoint(function(direction) {
+     // Highlight element when related content
+     // is 10% percent from the bottom... 
+     // remove if below
+     $('#myModal').modal('show');
+     getRelatedNavigation(this).toggleClass('active', direction === 'down');
+   }, {
+     offset: '90%' // 
+   })
 
-        }
+
+//  	$('.introduction').waypoint(function(event, direction) {
+//    if (direction === 'down') {
+//       // do this on the way down
+//       alert('asd');
+//    }
+//    else {
+//       // do this on the way back up through the waypoint
+//    offset: '50%'  // trigger at middle of page.
+
+//    }
+// });
 
 
- //$("#myModal").addClass("introduction");
- // alert('sdfs');
+		// $('.introduction').waypoint(function(event, direction) {
+		// 	//if (direction === 'down') {
 
-		$('.introduction').waypoint(function() {
-
-			//alert('test');
-			$('#myModal').modal('show');
-		    //alert('You have scrolled to my waypoint.');
+		// 	//alert('test');
+		// 	$('#myModal').modal('show');
+		// //}
+		//     //alert('You have scrolled to my waypoint.');
 		    
-		    //notify("Hello World");
-		}, {
-		    offset: '70%'
-		});
+		//     //notify("Hello World");
+		// }, {
+		//     offset: '70%'
+		// });
 
 
-function setinterval() {
-//alert("test");
-$('#myModal').modal('show');
-}
-var myTimer = setTimeout(setinterval, 5000);
+// function setinterval() {
+// //alert("test");
+// $('#myModal').modal('show');
+// }
+// var myTimer = setTimeout(setinterval, 5000);
 
 	});
 }(jQuery));
